e2243a1a44bf5fa924b9bde77d0c1184695cdafa
Anthony Rodriguez <anthony@nezia.xyz>
2021-01-29 19:12:49 +0100

    Add basic interface and the getGitCommand function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gitCommands from "./git-commands";
+const parseArgs = require("minimist");
+
+// Gets the matching git command from the git-commands.js file, and formats the description using the arguments if needed.
+//TODO: Get the list of boolean flags from the commands file, and make it command specific.
+//TODO: Also add a description of the flags.
+function getGitCommand(inputCommand) {
+  const parsedArgs = parseArgs(inputCommand.split(" "), { boolean: ["v"] });
+  const inputCommandName = parsedArgs._[1];
+  const matchingCommand = gitCommands.commands.find(
+    (command) => command.name === inputCommandName
+  );
+  if (!matchingCommand) {
+    return null;
+  }
+  matchingCommand.description = matchingCommand.description.replace(
+    "%s",
+    parsedArgs._.slice(2).join(", ")
+  );
+  return matchingCommand;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>What the git</h1>
+        <h2>Enter a git command and have it explained to you</h2>
+        <input type="text" className="input-command" />
       </header>
+      <div className="get-command-section">
+        <button className="get-command-button">Git it</button>
+      </div>
     </div>
   );
 }
