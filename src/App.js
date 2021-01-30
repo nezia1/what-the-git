8a9fb85b781c4fc1db229b5c29808d0f2f238ca8
Anthony Rodriguez <anthony@nezia.xyz>
2021-01-31 00:24:06 +0100

    Add parsing of arguments and getting descriptions for each of them

--- src/App.js
+++ src/App.js
@@ -6,15 +6,55 @@ const parseArgs = require("minimist");
 // Gets the matching git command from the git-commands.js file, and formats the description using the arguments if needed.
 //TODO: Get the list of boolean flags from the commands file, and make it command specific.
 //TODO: Also add a description of the flags.
+
 function getGitCommand(inputCommand) {
-  const parsedArgs = parseArgs(inputCommand.split(" "), { boolean: ["v"] });
-  const inputCommandName = parsedArgs._[1];
+  // Get the command name and check if it exists
+  const inputCommandName = inputCommand.split(" ")[1];
   const matchingCommand = gitCommands.commands.find(
     (command) => command.name === inputCommandName
   );
   if (!matchingCommand) {
     return null;
   }
+  // Get all the available boolean flags
+  const availableBooleanFlags = gitCommands.flags.filter((flag) =>
+    flag.commands.includes(inputCommandName)
+  );
+
+  // This array exists so it can be used with minimist (it takes the commands and their aliases to treat them as boolean flags when parsing)
+  const availableBooleanFlagsArray = availableBooleanFlags.reduce(
+    (acc, flag) => [...acc, flag.name, ...flag.aliases],
+    []
+  );
+
+  // Parse the arguments
+  const parsedArgs = parseArgs(inputCommand.split(" "), {
+    boolean: availableBooleanFlagsArray,
+  });
+
+  // Generate a list of flags with their corresponding descriptions
+  const flagsDescriptions = Object.entries(parsedArgs).reduce(
+    (acc, argument) => {
+      return [
+        ...acc,
+        // Filters out the boolean flags based on :
+        // If the flag exists
+        // If it's not _ (minimist prefix to store everything that's not a flag)
+        // If the flag is either stored as its full name or the alias
+        // TODO: add a check for duplicate
+        ...availableBooleanFlags.filter(
+          (flag) =>
+            argument[1] === true &&
+            argument[0] !== "_" &&
+            (flag.aliases.includes(argument[0]) || flag.name === argument[0])
+        ),
+      ];
+    },
+    []
+  );
+  console.log(flagsDescriptions);
+
+  // Generate a description based on the command and add a list of flags descriptions if needed
   const updatedMatchingCommand = {
     ...matchingCommand,
     description: matchingCommand.description.replace(
