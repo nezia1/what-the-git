64ba449ceed21087fac03c106710851c49f8b475
Anthony Rodriguez <anthony@nezia.xyz>
2021-02-01 19:49:30 +0100

    Remove deprecated commands array in flags

--- src/git-commands.js
+++ src/git-commands.js
@@ -9,20 +9,17 @@ const gitCommands = {
           aliases: ["v"],
           description:
             "Be verbose (allows the command to say more about what it does).",
-          commands: ["add"],
         },
         {
           name: "dry-run",
           aliases: ["n"],
           description:
             "Don’t actually add the file(s), just show if they exist.",
-          commands: ["add"],
         },
         {
           name: "force",
           aliases: ["f"],
           description: "Allow adding ignored files.",
-          commands: ["add"],
         },
       ],
     },
