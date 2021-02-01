198421395df2cab5a2781246251fafd191478311
Anthony Rodriguez <anthony@nezia.xyz>
2021-02-01 15:43:33 +0100

    Add new flags for git add

--- src/git-commands.js
+++ src/git-commands.js
@@ -10,9 +10,21 @@ const gitCommands = {
       name: "verbose",
       aliases: ["v"],
       description:
-        "Be verbose (allows the command to say more about what it does)",
+        "Be verbose (allows the command to say more about what it does).",
       commands: ["add"],
     },
+    {
+      name: "dry-run",
+      aliases: ["n"],
+      description: "Don’t actually add the file(s), just show if they exist.",
+      commands: ["add"]
+    },
+    {
+      name: "force",
+      aliases: ["f"],
+      description: "Allow adding ignored files.",
+      commands: ["add"]
+    }
   ],
 };
 
