f228780412ebe331d45f43d8a07b8c26217a068b
Anthony <anthony.rdrgz@pm.me>
2021-02-10 19:41:48 +0100

    Add git push (#7)
    * Add git push with the all flag
    
    * Add more flags to push

--- src/git-commands.js
+++ src/git-commands.js
@@ -52,6 +52,32 @@ const gitCommands = {
         },
       ],
     },
+    {
+      name: "push",
+      description: "Updates your remote branch with your local commits.",
+      flags: [
+        {
+          name: "all",
+          description: "Push all branches.",
+        },
+        {
+          name: "prune",
+          description:
+            "Remove remote branches that don’t have a local counterpart. For example, a branch foo will be removed if a local branch with the same name does not exist anymore.",
+        },
+        {
+          name: "dry-run",
+          aliases: ["n"],
+          description:
+            "Do everything except send the updates (effectively simulating a push).",
+        },
+        {
+          name: "porcelain",
+          description:
+            "Outputs machine-readable and parsable output (The output status line for each ref will be tab-separated and sent to stdout instead of stderr).",
+        },
+      ],
+    },
   ],
 };
 
