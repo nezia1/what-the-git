efc5e6d9938b530761faa078ff17ab115f0a3f09
Anthony Rodriguez <anthony@nezia.xyz>
2021-02-18 14:58:04 +0100

    Edit ParsedArguments interface and add SpecialTokens interface

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,20 @@
 export interface Flag {
-    name: string
-    aliases?: string[]
-    description: string
-    isString: boolean
+  name: string
+  aliases?: string[]
+  description: string
+  isString: boolean
 }
 
 export interface AvailableFlagsArray {
-    booleanFlagsArray: string[]
-    stringFlagsArray: string[]
+  booleanFlagsArray: string[]
+  stringFlagsArray: string[]
 }
 
 export interface ParsedArguments {
-    _: string
-    [argument: string]: string
+  _: string[]
+  flags: string[]
+}
+
+export interface SpecialTokens {
+  [key: string]: string
 }
