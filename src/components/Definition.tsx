ef1958a603fb2e23427523cc72a6a32d08df01f1
Anthony Rodriguez <anthony@nezia.xyz>
2021-02-25 09:17:08 +0100

    Implement the look of the Definition component

--- src/components/Definition.tsx
+++ src/components/Definition.tsx
@@ -1,5 +1,11 @@
 import { GitDefinition } from '../types'
+import './Definition.css'
 
 export default function Definition({ definition }: { definition: GitDefinition }) {
-  return <span>{definition.name}</span>
+  return (
+    <div className='definition'>
+      <span className='definition-term'>{definition.name}</span>
+      <div className='definition-box'>{definition.description}</div>
+    </div>
+  )
 }
