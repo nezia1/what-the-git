f9a488950dcc31ae6f2aec8f956cd57764c21cce
Anthony Rodriguez <anthony@nezia.xyz>
2021-03-01 13:53:55 +0100

    Use react-popper-tooltip for tooltips
    This library just makes it way easier to build tooltip. It adds a bit to the bundle size though, it might impact load times.

--- src/components/Definition.tsx
+++ src/components/Definition.tsx
@@ -1,13 +1,19 @@
+import { usePopperTooltip } from 'react-popper-tooltip'
 import { GitDefinition } from '../types'
 import './Definition.css'
 
 export default function Definition({ definition }: { definition: GitDefinition }) {
+  const { setTooltipRef, setTriggerRef, getTooltipProps, visible } = usePopperTooltip()
   return (
     <div className='definition'>
-      <span className='definition-term'>{definition.name}</span>
-      <article className='definition-box'>
-        <section>{definition.description}</section>
-      </article>
+      <span ref={setTriggerRef} className='definition-term'>
+        {definition.name}
+      </span>
+      {visible && (
+        <article ref={setTooltipRef} {...getTooltipProps({ className: 'definition-box' })}>
+          <section>{definition.description}</section>
+        </article>
+      )}
     </div>
   )
 }
