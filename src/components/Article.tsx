5445340ff0c160d47ee56947125b1ba0ae28b6bc
Anthony Rodriguez <anthony@nezia.xyz>
2021-03-02 13:31:41 +0100

    Add function to fetch article markdown

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,6 +1,17 @@
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 export default function Article() {
   const { article } = useParams<{ article: string }>()
-  return <div>{article}</div>
+  const [articleText, setArticleText] = useState('')
+
+  useEffect(() => {
+    async function getArticle() {
+      const response = await fetch(`${process.env.PUBLIC_URL}/markdown/${article}.md`)
+      const text = await response.text()
+      setArticleText(text)
+    }
+    getArticle()
+  }, [article])
+  return <div>{articleText}</div>
 }
